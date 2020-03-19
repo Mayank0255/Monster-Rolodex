b6349ea1c975d80c75258639daa430afafb92598
Mayank0255 <mayank2aggarwal@gmail.com>
2020-03-19 19:40:36 +0530

    Updates

---
+++ precache-manifest.f4be0b54a7ee2c2a1a7c2badff32d6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce58886def0f6db337a213cfdda5c6d7",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "ed959b79b6e7d58ea5da",
+    "url": "/Monster-Rolodex/static/css/main.c911b2c1.chunk.css"
+  },
+  {
+    "revision": "0d35040140e440a26978",
+    "url": "/Monster-Rolodex/static/js/2.1313f230.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodex/static/js/2.1313f230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed959b79b6e7d58ea5da",
+    "url": "/Monster-Rolodex/static/js/main.242a9b13.chunk.js"
+  },
+  {
+    "revision": "3e2b7460a7751f8973f3",
+    "url": "/Monster-Rolodex/static/js/runtime-main.1aff1f47.js"
+  }
+]);
